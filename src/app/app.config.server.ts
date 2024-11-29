abe6c52219dd194668713637c6ccb0d71c53aaf4
Gregory Arcentales <gregoarcenta@gmail.com>
2024-11-28 22:48:51 -0500

    prerender changed to false

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { provideServerRoutesConfig } from '@angular/ssr';
-import { serverRoutes } from '@/app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRoutesConfig(serverRoutes),
+    // provideServerRoutesConfig(serverRoutes),
   ],
 };
 
